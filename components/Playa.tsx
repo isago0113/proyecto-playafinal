266dcd8ed62267429eb4b2164ef5dff296f5c28d
isago0113 <isago0113@gmail.com>
2026-06-04 18:07:51 -0500

    arregloPelota2

--- components/Playa.tsx
+++ components/Playa.tsx
@@ -119,7 +119,6 @@ export default function Playa({ studentId }: PlayaProps) {
       />
 
       <Pelota
-        scene={scene}
         animando={animandoPelota}
         setAnimando={setAnimandoPelota}
       />
